159ff6c728b9488259f43514138cb41f520bad3a
Zaidan Bhat <zaidan.bhat@gmail.com>
2023-11-04 03:37:43 +0900

    Multithreaded Code

--- src/app.rs
+++ src/app.rs
@@ -1,31 +1,30 @@
 use std::collections::HashMap;
+use std::sync::Mutex;
 use rand::Rng;
 
 pub struct Restaurant {
-    tables: HashMap<u32, HashMap<String, (u32, u32)>>,
+    tables: Mutex<HashMap<u32, HashMap<String, (u32, u32)>>>,
 }
 
 impl Restaurant {
     pub fn new() -> Self {
         Restaurant {
-            tables: HashMap::new(),
+            tables: Mutex::new(HashMap::new()),
         }
     }
 
-    pub fn add_order(&self, item: String, table_number: u32, quantity: u32) -> Restaurant {
-        let mut new_tables = self.tables.clone(); // Clone to avoid mutation
+    pub fn add_order(&self, item: String, table_number: u32, quantity: u32) {
+        let mut new_tables = self.tables.lock().unwrap(); // Lock before accessing
         let table_entry = new_tables.entry(table_number).or_insert(HashMap::new());
         let mut rng = rand::thread_rng();
         let preparation_time = rng.gen_range(5..=15);
         let item_entry = table_entry.entry(item).or_insert((preparation_time, 0));
         let (_, current_quantity) = item_entry;
         *current_quantity += quantity;
-
-        Restaurant { tables: new_tables }
     }
 
-    pub fn remove_order(&self, table_number: u32, item_name: &str, quantity: u32) -> Restaurant {
-        let mut new_tables = self.tables.clone(); // Clone to avoid mutation
+    pub fn remove_order(&self, table_number: u32, item_name: &str, quantity: u32) {
+        let mut new_tables = self.tables.lock().unwrap(); // Lock before accessing
         if let Some(table_orders) = new_tables.get_mut(&table_number) {
             if let Some(item_entry) = table_orders.get_mut(item_name) {
                 let (_preparation_time, current_quantity) = item_entry;
@@ -36,12 +35,11 @@ impl Restaurant {
                 }
             }
         }
-
-        Restaurant { tables: new_tables }
     }
 
     pub fn query_table(&self, table_number: u32) {
-        if let Some(table_orders) = self.tables.get(&table_number) {
+        let tables = self.tables.lock().unwrap(); // Lock before accessing
+        if let Some(table_orders) = tables.get(&table_number) {
             println!("Orders for Table {}:", table_number);
             for (item_name, (preparation_time, quantity)) in table_orders.iter() {
                 println!("    * {}: {} ({})", item_name, quantity, preparation_time);
@@ -51,15 +49,13 @@ impl Restaurant {
         }
     }
 
-    
     pub fn print_all_orders(&self) {
-        for (table_number, table_orders) in self.tables.iter() {
+        let tables = self.tables.lock().unwrap(); // Lock before accessing
+        for (table_number, table_orders) in tables.iter() {
             println!("Table {}", table_number);
             for (item_name, (preparation_time, quantity)) in table_orders.iter() {
                 println!("    * {}: {} ({})", item_name, quantity, preparation_time);
             }
         }
     }
-    
 }
-
--- src/main.rs
+++ src/main.rs
@@ -1,17 +1,59 @@
-use crate::app::Restaurant;
+use std::thread;
+use std::sync::Arc;
 
 pub mod app;
+
 fn main() {
-    let restaurant = Restaurant::new();
+    let restaurant = Arc::new(app::Restaurant::new());
 
-    // Add some sample orders
-    let restaurant= restaurant.add_order(String::from("Pizza"), 1, 1);
-    let restaurant= restaurant.add_order(String::from("Pizza"), 1, 1);
-    let restaurant= restaurant.add_order(String::from("Salad"), 1, 1);
-    let restaurant= restaurant.add_order(String::from("Burger"), 2, 1);
+    // Spawn two threads to perform operations concurrently
+    let restaurant_clone = Arc::clone(&restaurant);
+    let thread1 = thread::spawn(move || {
+        restaurant_clone.add_order(String::from("Pizza"), 1, 1);
+    });
 
-    restaurant.print_all_orders();
+    let restaurant_clone = Arc::clone(&restaurant);
+    let thread2 = thread::spawn(move || {
+        restaurant_clone.add_order(String::from("Pizza"), 2, 1);
+    });
 
-    let restaurant= restaurant.remove_order(1, "Pizza", 1);
-    restaurant.query_table(1);
+    let restaurant_clone = Arc::clone(&restaurant);
+    let thread3 = thread::spawn(move || {
+        restaurant_clone.add_order(String::from("Pizza"), 3, 1);
+    });
+
+    let restaurant_clone = Arc::clone(&restaurant);
+    let thread4 = thread::spawn(move || {
+        restaurant_clone.add_order(String::from("Burger"), 2, 2);
+    });
+
+    let restaurant_clone = Arc::clone(&restaurant);
+    let thread5 = thread::spawn(move || {
+        restaurant_clone.add_order(String::from("Coffe"), 1, 1);
+    });
+
+
+
+    let restaurant_clone = Arc::clone(&restaurant);
+    let thread6 = thread::spawn(move || {
+        restaurant_clone.remove_order(1, "Pizza", 1);
+        restaurant_clone.query_table(1);
+    });
+
+    let restaurant_clone = Arc::clone(&restaurant);
+    let thread7= thread::spawn(move || {
+        restaurant_clone.print_all_orders();
+    });
+    // Wait for threads to finish
+    thread1.join().unwrap();
+    thread2.join().unwrap();
+    thread3.join().unwrap();
+    thread4.join().unwrap();
+    thread5.join().unwrap();
+    thread6.join().unwrap();
+    thread7.join().unwrap();
+    // thread8.join().unwrap();
+    // thread9.join().unwrap();
+    // thread10.join().unwrap();
+    // Print all orders after concurrent operations
 }
