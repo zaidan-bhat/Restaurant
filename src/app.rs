d089271fedcd5ec1a62bf71832196fe6298bb8f4
Zaidan Bhat <zaidan.bhat@gmail.com>
2023-11-04 16:34:01 +0900

    Added error handling by returning result type

--- src/app.rs
+++ src/app.rs
@@ -1,6 +1,13 @@
 use std::collections::HashMap;
 use rand::Rng;
 
+#[derive(Debug)]
+pub enum RestaurantError {
+    TableNotFound,
+    ItemNotFound,
+    InsufficientQuantity,
+}
+
 pub struct Restaurant {
     tables: HashMap<u32, HashMap<String, (u32, u32)>>,
 }
@@ -12,7 +19,7 @@ impl Restaurant {
         }
     }
 
-    pub fn add_order(&self, item: String, table_number: u32, quantity: u32) -> Restaurant {
+    pub fn add_order(&self, item: String, table_number: u32, quantity: u32) -> Result<Restaurant, RestaurantError> {
         let mut new_tables = self.tables.clone(); // Clone to avoid mutation
         let table_entry = new_tables.entry(table_number).or_insert(HashMap::new());
         let mut rng = rand::thread_rng();
@@ -21,11 +28,12 @@ impl Restaurant {
         let (_, current_quantity) = item_entry;
         *current_quantity += quantity;
 
-        Restaurant { tables: new_tables }
+        Ok(Restaurant { tables: new_tables })
     }
 
-    pub fn remove_order(&self, table_number: u32, item_name: &str, quantity: u32) -> Restaurant {
+    pub fn remove_order(&self, table_number: u32, item_name: &str, quantity: u32) -> Result<Restaurant, RestaurantError> {
         let mut new_tables = self.tables.clone(); // Clone to avoid mutation
+        
         if let Some(table_orders) = new_tables.get_mut(&table_number) {
             if let Some(item_entry) = table_orders.get_mut(item_name) {
                 let (_preparation_time, current_quantity) = item_entry;
@@ -34,32 +42,56 @@ impl Restaurant {
                 } else {
                     table_orders.remove(item_name);
                 }
+                Ok(Restaurant { tables: new_tables })
+            } else {
+                Err(RestaurantError::ItemNotFound)
             }
+        } else {
+            Err(RestaurantError::TableNotFound)
         }
-
-        Restaurant { tables: new_tables }
     }
 
-    pub fn query_table(&self, table_number: u32) {
+    pub fn query_item_for_table(&self, table_number: u32, item_name: &str) -> Result<(), RestaurantError> {
+        if let Some(table_orders) = self.tables.get(&table_number) {
+            if let Some((preparation_time, quantity)) = table_orders.get(item_name) {
+                println!(
+                    "Details for Item '{}' at Table {}:\n    Quantity: {}\n    Preparation Time: {}",
+                    item_name, table_number, quantity, preparation_time
+                );
+                Ok(())
+            } else {
+                Err(RestaurantError::ItemNotFound)
+            }
+        } else {
+            Err(RestaurantError::TableNotFound)
+        }
+    }
+    
+    pub fn query_table(&self, table_number: u32) -> Result<(), RestaurantError> {
         if let Some(table_orders) = self.tables.get(&table_number) {
             println!("Orders for Table {}:", table_number);
             for (item_name, (preparation_time, quantity)) in table_orders.iter() {
-                println!("    * {}: {} ({})", item_name, quantity, preparation_time);
+                println!(
+                    "    * {}: Quantity - {}, Preparation Time - {}",
+                    item_name, quantity, preparation_time
+                );
             }
+            Ok(())
         } else {
-            println!("Table {} not found.", table_number);
+            Err(RestaurantError::TableNotFound)
         }
     }
 
-    
     pub fn print_all_orders(&self) {
         for (table_number, table_orders) in self.tables.iter() {
             println!("Table {}", table_number);
             for (item_name, (preparation_time, quantity)) in table_orders.iter() {
-                println!("    * {}: {} ({})", item_name, quantity, preparation_time);
+                println!(
+                    "    * {}: Quantity - {}, Preparation Time - {}",
+                    item_name, quantity, preparation_time
+                );
             }
         }
     }
-    
 }
 
--- src/main.rs
+++ src/main.rs
@@ -5,13 +5,14 @@ fn main() {
     let restaurant = Restaurant::new();
 
     // Add some sample orders
-    let restaurant= restaurant.add_order(String::from("Pizza"), 1, 1);
-    let restaurant= restaurant.add_order(String::from("Pizza"), 1, 1);
-    let restaurant= restaurant.add_order(String::from("Salad"), 1, 1);
-    let restaurant= restaurant.add_order(String::from("Burger"), 2, 1);
+    let restaurant= restaurant.add_order(String::from("Pizza"), 1, 1).unwrap();
+    let restaurant= restaurant.add_order(String::from("Pizza"), 1, 1).unwrap();
+    let restaurant= restaurant.add_order(String::from("Salad"), 1, 1).unwrap();
+    let restaurant= restaurant.add_order(String::from("Burger"), 2, 1).unwrap();
 
     restaurant.print_all_orders();
 
-    let restaurant= restaurant.remove_order(1, "Pizza", 1);
-    restaurant.query_table(1);
+    let restaurant= restaurant.remove_order(1, "Pizza", 1).unwrap();
+    let _result= restaurant.query_table(1);
+    let _result= restaurant.query_item_for_table(2, "Burger");
 }
